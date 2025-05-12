11cdc927fbc6302d67240858171919fe94078841
Maxim Rebguns <mrmaxguns@gmail.com>
2025-05-12 14:25:36 -0500

    allow typing enter for default values, general refactor

--- src/jsongen.cpp
+++ src/jsongen.cpp
@@ -3,62 +3,62 @@
 
 using namespace std;
 
+int get_number(const int default_value) {
+    string input;
+    getline(cin, input);
+    if (input.empty()) {
+        return default_value;
+    }
+    return stoi(input);
+}
+
 int main() {
     ofstream file("input.json");
-    // deciding cores
+
+    // Core classes
     file << "{" << endl << "    \"cores\": [";
-    std::cout << "how many cores? (default 4)" << endl;
-    int cores;
-    std::cin >> cores;
-    if (!cores) {
-        cores = 4;
-    }
+    cout << "how many cores? (default 4) ";
+    int cores = get_number(4);
+
     for (int i = 0; i < cores-1; i++) {
         file << "\"Core" << i + 1 << "\", ";
     }
     file << "\"Core" << cores << "\"";
     file << "]," << endl;
-    // deciding electives
+
+    // Electives
     file << "\n    \"electives\": [";
-    std::cout << "how many Electives? (default 6)" << endl;
-    int electives;
-    std::cin >> electives;
-    if (!electives) {
-        electives = 6;
-    }
+    std::cout << "how many Electives? (default 6) ";
+    int electives = get_number(6);
+
     for (int i = 0; i < electives-1; i++) {
         file << "\"Elective" << i + 1 << "\", ";
     }
     file << "\"Elective" << electives << "\"";
     file << "]," << endl;
-    // deciding students
+
+    // Students
     file << "\n    \"students\": [";
-    int students;
-    std::cout << endl << "how many Students? (default 20)" << endl;
-    std::cin >> students;
-    if (!students) {
-        students = 20;
-    }
-    int electives_per_student;
-    std::cout << endl << "how many desired electives per student? (default 4, be less than # of electives)" << endl;
-    std::cin >> electives_per_student;
-    if (!electives_per_student) {
-        electives_per_student = 4;
-    }
+    std::cout << "how many Students? (default 20) ";
+    int students = get_number(20);
+
+    std::cout << "how many desired electives per student? (default 4, be less than # of electives) ";
+    int electives_per_student = get_number(4);
+
     for (int i = 0; i < students; i++) {
         if (i == 0) file << "\n        {\"id\":   " << i + 1 << ", \"courses\": [";
         else        file << ",\n        {\"id\":   " << i + 1 << ", \"courses\": [";
-        int* electiveList =(int*)malloc(sizeof(int) * electives_per_student); // array to electives
+        int* electiveList =(int*)malloc(sizeof(int) * electives_per_student); // Array to electives
         for (int j = 0; j < electives_per_student; j++) {
-            // creates elective values for each student
-            int randNum = rand() % electives + 1; // generates a random number within random elective bounds
+            // Creates elective values for each student
+            int randNum = rand() % electives + 1; // Generates a random number within random elective bounds
             bool alreadyThere = false;
             for (int k = 0; k < j; k++) {
-                // iterates through all already-generated indicies
+                // Iterates through all already-generated indices
                 if (randNum == *(electiveList + k)) {
-                    // checks the indicies to make sure the same elective isn't generated twice
+                    // Checks the indices to make sure the same elective isn't generated twice
                     j--;
-                    alreadyThere = true; // if it is generated twice, repeat the generation
+                    alreadyThere = true; // If the same number generated twice, repeat the generation
                 }
             }
             if (alreadyThere) continue;
@@ -71,4 +71,7 @@ int main() {
     }
 
     file << "\n   ]\n}";
+
+    file.close();
+    return 0;
 }
