ea41ac4cbb6be30d8fc120296544515d490d4562
Cole_Henson <acolehenson@gmail.com>
2025-05-18 20:04:52 -0500

    hopefully fixed cmake checks?

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -27,7 +27,13 @@ vector<Schedule> Mutation(vector<Schedule> genome) //mutates schedules
     int pop = genome.size();//number of schedules
     Schedule temp_sch;
     double rand;
-    
+    uniform_int_distribution<int> uid(0,7);//7 or however many periods we have - 1
+    default_random_engine dre;
+
+    for(Schedule s: genome)
+    {
+
+    }
     return mut_sch;
 }
 /*makes a new list of schedules by combing students from the genome based on the relative fitness of those genomes*/
@@ -38,14 +44,14 @@ vector<Schedule> genetic_crossover(vector<Schedule> genome)
     int pop = genome.size();//number of schedules
     double sum = 0;
     int index = 0;
-    Schedule temp_sch;
     double rand;
     uniform_real_distribution<double> urd(0,1);//distribution of real numbers from 0-1 
     default_random_engine dre;//this is how I am selecting a random number from urd
     vector<string> students;
-    genome[0].get_all_students(students);
+    genome[0].get_all_students(students);// the idea is that when I am making my new schedule I just add entire students
     for(int i = 0; i < pop; i++)
     {
+        //still need to have a way to combine schedules such that the balance of # of students per class is maintained.
         for(string s: students)
         {
             rand = urd(dre);//generates a random number
@@ -65,5 +71,10 @@ vector<Schedule> genetic_crossover(vector<Schedule> genome)
             index = 0;
         }
     }
+    //for(Schedule sched: genome)//clear up space genome was taking up
+    //{
+    //    delete sched; //incomplete type. need to fix.
+    //}
+    //genome.clear();
     return offspring;
 }
--- src/mutation.h
+++ src/mutation.h
@@ -1,9 +1,6 @@
-#ifndef mutation.h
-#define mutation.h
-
 #include <vector>
 #include "schedule.h"
-#include "json.h"
+#include "json/json.h"
 #include "fitness.h"
 #include <random>
 
@@ -13,6 +10,4 @@ using namespace std;
 vector<Schedule> Mutation(vector<Schedule> schedules); //mutates schedules
 vector<double> prob_helper(vector<Schedule> schedules);
 vector<Schedule> genetic_crossover(vector<Schedule> genome);
-//void move_student(Student stndt);
-
-#endif
+//void move_student(Student stndt);take a student from one schedule and put it in a different schedule. doesn't matter if the first schedule is altered or not, we are probably going to be deleting it anyway.
